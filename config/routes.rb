8c5356566a7e2eab94f1fc41cc10c62168e94997
Iren Liber <irr.morgo@gmail.com>
2016-05-12 15:40:20 +0600

    created events_controller and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'events/index'
+
   get 'calendar/index'
 
   root 'calendar#index'
+
+  resources :events
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
