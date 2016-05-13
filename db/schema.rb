e270569d8411fd147046bfa069b7c4673fb7f600
Iren Liber <irr.morgo@gmail.com>
2016-05-13 20:25:59 +0600

    created migration, added a column "date" to the table "Events"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512132009) do
+ActiveRecord::Schema.define(version: 20160513141926) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160512132009) do
     t.boolean  "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "date"
   end
 
 end
