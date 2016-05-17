92db7d3f4c8d843100195cf272d954dd1441985b
Iren Liber <irr.morgo@gmail.com>
2016-05-17 21:42:45 +0600

    added new methods to events_controller: show, new, create, edit, update, destroy, event_params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,38 @@ class EventsController < ApplicationController
   def index
     @events = Event.where(date: params[:date]).order(date_start: :desc)
   end
+
+  def show
+
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location)
+  end
+
 end
