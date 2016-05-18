6823b09484247f57443f992a28ecaad07bc847f2
Iren Liber <irr.morgo@gmail.com>
2016-05-18 17:30:28 +0600

    changed events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.where(date: params[:date]).order(date_start: :desc)
+    if params[:date].blank?
+      @events = Event.order ("date_start DESC")
+    else
+      @date = params[:date]
+      @events = Event.where(date_start: @date).order("start_time ASC")
+    end
   end
 
   def show
@@ -15,7 +20,7 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
 
     if @event.save
-      redirect_to @event
+      redirect_to events_path(:date => event_params[:date_start])
     else
       render 'new'
     end
@@ -25,15 +30,22 @@ class EventsController < ApplicationController
   end
 
   def update
+    if @event.update(event_params)
+      redirect_to events_path(:date => event_params[:date_start])
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @event.destroy
+    redirect_to events_path(:date => params[:date])
   end
 
   private
 
   def event_params
-    params.require(:event).permit(:title, :location)
+    params.require(:event).permit(:title, :location, :date_start, :date_end, :start_time, :end_time, :done)
   end
 
 end
