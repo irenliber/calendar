1e821f99eec36e220acbd8dae6080feeeec5e5f7
irenliber <irr.morgo@gmail.com>
2016-05-24 11:59:19 +0600

    added jquery-timepicker and bootstrap-datepicker to application.js and application.scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require bootstrap-datepicker
+//= require jquery.timepicker.js
 //= require turbolinks
 //= require_tree .
